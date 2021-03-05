1c7937f4b6e3dc42fdb79cac1431fcb675844eca
Adi Andrea <dekz.itfz@gmail.com>
2021-03-05 12:49:16 +0800

    basic timer implementation

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,26 +16,25 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
+@ExperimentalAnimationApi
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,57 +46,126 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+@Composable
+fun Progress(value: Double) {
+    CircularProgressIndicator(
+        progress = value.toFloat(),
+        modifier = Modifier
+            .width(200.dp)
+            .height(200.dp)
+    )
+}
+
 // Start building your app here!
+@ExperimentalAnimationApi
 @Composable
 fun MyApp() {
+    val isRunning = remember { mutableStateOf(false) }
+    val timerProgress = remember { mutableStateOf(1.0) }
+    val timerTimeInMillis = remember { mutableStateOf(0L) }
+
+    val hour = remember { mutableStateOf("0")}
+    val minute = remember { mutableStateOf("0")}
+    val second = remember { mutableStateOf("0")}
+
     Surface(
         color = MaterialTheme.colors.background,
         modifier = Modifier.padding(8.dp)
     ) {
-        Row {
-            val hour = remember { mutableStateOf("0")}
-            val minute = remember { mutableStateOf("0")}
-            val second = remember { mutableStateOf("0")}
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row {
 
-            OutlinedTextField(
-                value = hour.value,
-                onValueChange = { hour.value = it },
-                label = { Text(text = "Hours") },
-                singleLine = true,
-                modifier = Modifier.weight(0.3f),
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Number,
-                    autoCorrect = false
+                OutlinedTextField(
+                    value = hour.value,
+                    onValueChange = { hour.value = it },
+                    label = { Text(text = "Hours") },
+                    singleLine = true,
+                    modifier = Modifier.weight(0.3f),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number,
+                        autoCorrect = false
+                    )
                 )
-            )
-            Spacer(Modifier.width(4.dp))
-            OutlinedTextField(
-                value = minute.value,
-                onValueChange = { minute.value = it },
-                label = { Text(text = "Minutes") },
-                singleLine = true,
-                modifier = Modifier.weight(0.3f),
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Number,
-                    autoCorrect = false
+                Spacer(Modifier.width(4.dp))
+                OutlinedTextField(
+                    value = minute.value,
+                    onValueChange = { minute.value = it },
+                    label = { Text(text = "Minutes") },
+                    singleLine = true,
+                    modifier = Modifier.weight(0.3f),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number,
+                        autoCorrect = false
+                    )
                 )
-            )
-            Spacer(Modifier.width(4.dp))
-            OutlinedTextField(
-                value = second.value,
-                onValueChange = { second.value = it },
-                label = { Text(text = "Seconds") },
-                singleLine = true,
-                modifier = Modifier.weight(0.3f),
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Number,
-                    autoCorrect = false
+                Spacer(Modifier.width(4.dp))
+                OutlinedTextField(
+                    value = second.value,
+                    onValueChange = { second.value = it },
+                    label = { Text(text = "Seconds") },
+                    singleLine = true,
+                    modifier = Modifier.weight(0.3f),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number,
+                        autoCorrect = false
+                    )
                 )
-            )
+            }
+
+            Spacer(Modifier.height(8.dp))
+
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    val totalTime: Long = (second.value.toLong() * 1000) + (minute.value.toLong() * 60000) + (hour.value.toLong() * 3600000)
+                    timerTimeInMillis.value = totalTime
+
+                    val timer = object: CountDownTimer(timerTimeInMillis.value, 1000){
+                        override fun onTick(millisUntilFinished: Long) {
+                            val remainingSeconds: Double = millisUntilFinished.toDouble() / 1000
+                            val currentProgress: Double = remainingSeconds/(timerTimeInMillis.value / 1000)
+                            timerProgress.value = currentProgress
+                        }
+                        override fun onFinish() {
+                            isRunning.value = false
+                        }
+                    }
+
+                    isRunning.value = !isRunning.value
+                    if(isRunning.value){
+                        timer.start()
+                    }else{
+                        timerTimeInMillis.value = 0L
+                        timerProgress.value = 1.0
+                        timer.onFinish()
+                        timer.cancel()
+                    }
+                }
+            ) {
+                if(isRunning.value){
+                    Text("Stop")
+                }else{
+                    Text("Start")
+                }
+            }
+
+            Spacer(Modifier.height(18.dp))
+
+            AnimatedVisibility(
+                visible = isRunning.value,
+                enter = fadeIn(initialAlpha = 0.3f),
+                exit = fadeOut(animationSpec = tween(durationMillis = 500))
+            ) {
+                Progress(timerProgress.value)
+            }
         }
+
     }
 }
 
+@ExperimentalAnimationApi
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun LightPreview() {
@@ -106,6 +174,7 @@ fun LightPreview() {
     }
 }
 
+@ExperimentalAnimationApi
 @Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun DarkPreview() {
