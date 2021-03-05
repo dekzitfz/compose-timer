bc425aadd93b5de986ceebbfd973e97d526c2c7e
Adi Andrea <dekz.itfz@gmail.com>
2021-03-05 10:29:44 +0800

    create time input

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,11 +18,22 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -39,8 +50,51 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+    Surface(
+        color = MaterialTheme.colors.background,
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Row {
+            val hour = remember { mutableStateOf("0")}
+            val minute = remember { mutableStateOf("0")}
+            val second = remember { mutableStateOf("0")}
+
+            OutlinedTextField(
+                value = hour.value,
+                onValueChange = { hour.value = it },
+                label = { Text(text = "Hours") },
+                singleLine = true,
+                modifier = Modifier.weight(0.3f),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    autoCorrect = false
+                )
+            )
+            Spacer(Modifier.width(4.dp))
+            OutlinedTextField(
+                value = minute.value,
+                onValueChange = { minute.value = it },
+                label = { Text(text = "Minutes") },
+                singleLine = true,
+                modifier = Modifier.weight(0.3f),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    autoCorrect = false
+                )
+            )
+            Spacer(Modifier.width(4.dp))
+            OutlinedTextField(
+                value = second.value,
+                onValueChange = { second.value = it },
+                label = { Text(text = "Seconds") },
+                singleLine = true,
+                modifier = Modifier.weight(0.3f),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    autoCorrect = false
+                )
+            )
+        }
     }
 }
 
